cd6ef947e326ad579d1e13de27371adf06c66318
Alexander Urizar <urizaraj@gmail.com>
2018-04-02 17:11:25 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -48,7 +48,7 @@ class SongsController < ApplicationController
 
   def upload
     CSV.foreach(params[:leads].path, headers: true) do |lead|
-      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+      Song.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
     redirect_to customers_path
   end
